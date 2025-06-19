3690e1508b7cf4af7ea9047e22e65b79b2891aca
Math012 <math111.0811@gmail.com>
2025-06-19 09:50:46 -0300

    Adicionado os metodos de envio de email

---
+++ src/main/java/com/math012/notificacaoapi/business/dto/TarefasDTO.java
@@ -0,0 +1,26 @@
+package com.math012.notificacaoapi.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.math012.notificacaoapi.business.enums.StatusTarefa;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TarefasDTO {
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataAlteracao;
+    private StatusTarefa statusTarefa;
+}
